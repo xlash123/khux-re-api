27dce4611b55adb788a845866e430b4d487711b6
Noah Gil <xlash19o13@gmail.com>
2020-06-26 00:44:56 -0400

    Decypted application/enoded-json

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const https = require('https');
 const fs = require('fs');
+const aes = require('aes-js');
+const { b64 } = require('js-base64');
 const { gunzipSync } = require('zlib');
 
 https.globalAgent.options.ca = require('ssl-root-cas/latest').create();
@@ -8,9 +10,22 @@ const port = 443;
 
 let responseCounter = 0;
 
+// This is the AES-256 key that encrypts the JSON
+// It is fetched during a session grab
+let sharedSecurityKey;
+
 const ssl = {
 	key: fs.readFileSync('key.pem', 'utf8'),
     cert: fs.readFileSync('cert.pem', 'utf8')
+}
+
+// Returns a decrypted JSON object
+function decryptJson(encodedB64, secretKey) {
+	const keyBytes = Buffer.from(secretKey);
+	const aesCbc = new aes.ModeOfOperation.cbc(keyBytes);
+	const decodedBuffer = Buffer.from(Base64.atob(encodedB64), 'binary');
+	const decryptedBytes = aesCbc.decrypt(decodedBuffer);
+	return aes.utils.utf8.fromBytes(decryptedBytes).trim();
 }
 
 const server = https.createServer(ssl, (req, res) => {
@@ -42,15 +57,22 @@ const server = https.createServer(ssl, (req, res) => {
 		});
 		pRes.on('end', () => {
 			if (isGzip) {
+				body = gunzipSync(body).toString();
 				if (isEncJson) {
-					fs.writeFileSync(`packets/${responseCounter++}.gz`, body, 'binary');
-					console.log('wrote as' + (responseCounter - 1));
+
+					body = decryptJson(body, sharedSecurityKey);
 				}
-				body = gunzipSync(body).toString().trim();
 			}
 			try {
 				body = JSON.parse(body.toString());
 			} catch (e) {}
+
+			// Log the AES key when we get it
+			if (typeof body === 'object') {
+				if (body.sharedSecurityKey) {
+					sharedSecurityKey = body.sharedSecurityKey;
+				}
+			}
 
 			console.log('From SE', {
 				status: res.statusCode,
